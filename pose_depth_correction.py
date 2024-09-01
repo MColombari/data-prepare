5f6c0671e68301f38a42062645bad19e2311e18f
omaymaMoussadek <omy.moussadek@gmail.com>
2024-09-01 14:48:56 +0200

    test different angles for 3d

--- pose_depth_correction.py
+++ pose_depth_correction.py
@@ -12,10 +12,11 @@ def calculate_rotation_angle(joint1, joint2):
     delta_x = joint2[0] - joint1[0]
     delta_y = joint2[1] - joint1[1]
     delta_z = joint2[2] - joint1[2]
-    #print(f"punto1_xyz: {joint1[0]}, {joint1[1]}, {joint1[2]}")
-    #print(f"punto2_xyz: {joint2[0]}, {joint2[1]}, {joint2[2]}")
     
-    #print(f"delta_x:{delta_x}, delta_y={delta_y}, delta_z:{delta_z}")
+    #with open('/homes/omoussadek/CV/SLR_Sentiment_Enhanced/data-prepare/for_angle_analysis/bin10.txt', 'a') as file:
+    #    print(f"Left_Shoulder_xyz: {joint1[0]}, {joint1[1]}, {joint1[2]}",file=file)
+    #    print(f"Right_Shoulder_xyz: {joint2[0]}, {joint2[1]}, {joint2[2]}",file=file)
+    #    print(f"delta_x:{delta_x}, delta_y={delta_y}, delta_z:{delta_z}",file=file)
     
     angle_no_deepth = np.arctan2(delta_y, delta_x) * 180 / np.pi
     angle_with_deepth = np.arctan2(delta_z, delta_x) * 180 / np.pi
@@ -42,21 +43,20 @@ def calculate_rotation_angle(joint1, joint2):
     #print(f"angle_no_depth:{angle_no_deepth}, angle_with_depth={angle_with_deepth}, angle_for_correction:{angle_for_correction}")
     return angle_no_deepth, angle_with_deepth, angle_for_correction
 
-def rotate_joints_3d(joints, angle_x, angle_y, center):
-    angle_x_rad = np.deg2rad(angle_x)
+def rotate_joints_3d(joints, angle_z, angle_y, center):
+    angle_z_rad = np.deg2rad(angle_z)
     angle_y_rad = np.deg2rad(angle_y)
 
-    # Matrici di rotazione 3D
-    rot_x = np.array([[1, 0, 0],
-                      [0, np.cos(angle_x_rad), -np.sin(angle_x_rad)],
-                      [0, np.sin(angle_x_rad), np.cos(angle_x_rad)]])
+    rot_z = np.array([[np.cos(angle_z_rad), -np.sin(angle_z_rad), 0],
+                  [np.sin(angle_z_rad), np.cos(angle_z_rad), 0],
+                  [0, 0, 1]])
 
     rot_y = np.array([[np.cos(angle_y_rad), 0, np.sin(angle_y_rad)],
                       [0, 1, 0],
                       [-np.sin(angle_y_rad), 0, np.cos(angle_y_rad)]])
 
-    # Rotazione combinata intorno agli assi X e Y
-    rot_matrix = np.dot(rot_y, rot_x)
+    # risp al current frame post moltiplicazione: prima ruoto la z (2d) e poi la y (depth)
+    rot_matrix = np.dot(rot_z, rot_y)
 
     # Traslazione giunti al centro
     translated_joints = joints - np.array(center)
@@ -96,7 +96,6 @@ for root, dirs, files in os.walk(npy_folder, topdown=False):
         new_npy_depth = npy.copy()
         
         correctionIndex = 1
-        print(f"\n{name}")
         
         all_frames_angles= np.zeros((npy.shape[0],3))
         
@@ -105,15 +104,15 @@ for root, dirs, files in os.walk(npy_folder, topdown=False):
         
         angle_discretized = np.zeros(3)
         
-        bins_angle= np.zeros((18,3)) #ho 18 bin da 10 gradi ciascuno
+        bins_angle= np.zeros((36,3)) #ho 18 bin da 10 gradi ciascuno
         
         #salva per tutti i frame i bin incrementati in base all'angolo
         for k in range(npy.shape[0]):
             all_frames_angles[k] = calculate_rotation_angle(left_shoulder[k], right_shoulder[k])
             
-            bin_no_depth = int(all_frames_angles[k][0] // 10)  #primo angolo
-            bin_with_depth = int(all_frames_angles[k][1] // 10) #secondo angolo
-            bin_for_correction = int(all_frames_angles[k][2] // 10) #terzo angolo
+            bin_no_depth = int(all_frames_angles[k][0] // 5)  #primo angolo
+            bin_with_depth = int(all_frames_angles[k][1] // 5) #secondo angolo
+            bin_for_correction = int(all_frames_angles[k][2] // 5) #terzo angolo
             
             if bin_no_depth >= 0:
                bins_angle[bin_no_depth,0] += 1
@@ -133,10 +132,10 @@ for root, dirs, files in os.walk(npy_folder, topdown=False):
         indices_max = np.argmax(bins_angle, axis=0)
         
         for i in range(3):
-            if 0 <= indices_max[i] <= 8:
-                angle_discretized[i] = indices_max[i] * 10
+            if 0 <= indices_max[i] <= 17:
+                angle_discretized[i] = indices_max[i] * 5
             else:
-                angle_discretized[i] = (18 - indices_max[i]) * -10
+                angle_discretized[i] = (36 - indices_max[i]) * -5
 
         #angle_video[:]= mode(all_frames_angles, axis=0,  keepdims=False).mode[0] #-->trova i valori usando la moda
         #median_angle[:]= np.median(all_frames_angles, axis=0) #--> trova i valori usando la mediana
@@ -144,21 +143,24 @@ for root, dirs, files in os.walk(npy_folder, topdown=False):
         delta_x = left_shoulder[1][0] - right_shoulder[1][0]
         delta_y = left_shoulder[1][1] - right_shoulder[1][1]
         delta_z = left_shoulder[1][2] - right_shoulder[1][2]
-        print(f"Right Shoulder coord: {right_shoulder[1][0]}, {right_shoulder[1][1]}, {right_shoulder[1][2]}")
-        print(f"Left Shoulder coord: {left_shoulder[1][0]}, {left_shoulder[1][1]}, {left_shoulder[1][2]}")
-    
-        print(f"delta_x:{delta_x}, delta_y={delta_y}, delta_z:{delta_z}")
-        print(f"angle discretized: {angle_discretized}")
+        #with open('/homes/omoussadek/CV/SLR_Sentiment_Enhanced/data-prepare/for_angle_analysis/bin30_sh.txt', 'a') as file:
+        #    print(f"\n\n{name}\n", file=file)
+        #    print(f"Right Shoulder coord: {right_shoulder[1][0]}, {right_shoulder[1][1]}, {right_shoulder[1][2]}", file=file)
+        #    print(f"Left Shoulder coord: {left_shoulder[1][0]}, {left_shoulder[1][1]}, {left_shoulder[1][2]}",file=file)
+        #    print(f"delta_x:{delta_x}, delta_y={delta_y}, delta_z:{delta_z}",file=file)
+        #    print(f"angle discretized: {angle_discretized}",file=file)
         #print(f"Frequent angle value in video (Moda):{angle_video}")
         #print(f"Central angle values in video (Mediana):{median_angle}")
         
-        for i in range(0):
+    
+        for i in range(npy.shape[0]):
             
             depth_pxl = left_shoulder[i,2] - right_shoulder [i,2] # è delta z
             delta_x= left_shoulder[i,0] - right_shoulder [i,0]
             delta_y = left_shoulder[i,1] - right_shoulder [i,1] 
             
             angle_no_depth, angle_with_depth, angle = calculate_rotation_angle(left_shoulder[i], right_shoulder[i])
+            
             
             #epsilon = 1e-5  # Tolleranza per il confronto
             #print(angle_with_depth)
@@ -180,18 +182,18 @@ for root, dirs, files in os.walk(npy_folder, topdown=False):
             distXY = calculate_distance_3d(left_shoulder_xy, right_shoulder_xy)
 
             #print(distXY)
-            condition = (depth_pxl == (correctionIndex * np.sin(angle_with_depth)*distXZ))
-            #print(correctionIndex * np.sin(angle_with_depth)*distXY)
-            #print(depth_pxl)
-            
-            
-            if depth_pxl and not (condition):
-                #print("dentro")
-                #print(angle)
-                #print(angle_with_depth)
-                angle_with_depth = angle
-                correctionIndex = depth_pxl / (np.sin(angle_with_depth) * distXY)
-                #print(correctionIndex)
+            #condition = (depth_pxl == (correctionIndex * np.sin(angle_with_depth)*distXZ))
+            ##print(correctionIndex * np.sin(angle_with_depth)*distXY)
+            ##print(depth_pxl)
+            #
+            #
+            #if depth_pxl and not (condition):
+            #    #print("dentro")
+            #    #print(angle)
+            #    #print(angle_with_depth)
+            #    #angle_with_depth = angle
+            #    correctionIndex = depth_pxl / (np.sin(angle_with_depth) * distXY)
+            #    #print(correctionIndex)
 
             #print(angle)
             #print(angle_with_depth)
@@ -199,9 +201,11 @@ for root, dirs, files in os.walk(npy_folder, topdown=False):
             
             #printa quelli per cui non è rispettata questa cosa
             
+            
+            
             center3D = (int(center_x[i]), int(center_y[i]),int(center_z[i]))
-            rotated3D_shoulders = rotate_joints_3d(npy[i, shoulder_joints, :3], angle_no_depth ,angle_with_depth, center3D) #ruoto anche rispetto alla profondità
-            new_npy_depth[i, shoulder_joints, :] = rotated3D_shoulders
+            rotated3D_shoulders = rotate_joints_3d(npy[i, :, :3], angle_discretized[0]  ,angle_discretized[1], center3D) #ruoto anche rispetto alla profondità
+            new_npy_depth[i, :, :] = rotated3D_shoulders
             
             #calcolo distanza sul piano xy prima della rotazione e post rotazione e se è diversa aggiusto le coordinate di un fattore moltiplicativo
             
@@ -211,7 +215,7 @@ for root, dirs, files in os.walk(npy_folder, topdown=False):
         npy = np.load(os.path.join(npy_folder, name)).astype(np.float32)
         new_npy_depth[:, :, 2] = npy[:, :, 3]
             
-        #np.save(os.path.join(out_folder, name[:-4] + '.npy'), new_npy_depth)
+        np.save(os.path.join(out_folder, name[:-4] + '.npy'), new_npy_depth)
         
         
             
